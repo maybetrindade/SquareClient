4f8cc1d2d1c657dea0659c046aa156e4959ed241
Felipe Teixeira <secretedit123e@gmail.com>
2024-11-12 21:22:12 -0300

    fix: return error code correctly when trying to fetch an account

--- app/src/main/java/com/teixeira0x/squareclient/data/repository/AccountRepositoryImpl.kt
+++ app/src/main/java/com/teixeira0x/squareclient/data/repository/AccountRepositoryImpl.kt
@@ -8,27 +8,37 @@ import com.teixeira0x.squareclient.domain.repository.AccountRepository
 import java.net.HttpURLConnection
 import javax.inject.Inject
 
-/** @author Felipe Teixeira */
+/**
+ * Implementation of the account repository.
+ *
+ * @author Felipe Teixeira
+ */
 class AccountRepositoryImpl
 @Inject
 constructor(private val service: SquareService) : AccountRepository {
 
   override suspend fun fetchAccount(token: String): Result<Account> {
-    try {
-      val response = service.fetchAccount(token).body()
-      if (response?.status == "success") {
-        val account = response.response!!.toAccount()
-        return Result.success(account)
+    return try {
+      val response = service.fetchAccount(token)
+
+      if (response.isSuccessful && response.body()?.status == "success") {
+        val account = response.body()?.response?.toAccount()
+        Result.success(account!!)
+      } else {
+        Result.failure(
+          RequestException(
+            message = "Unable to fetch account",
+            code = response.code(),
+          )
+        )
       }
     } catch (th: Throwable) {
-      th.printStackTrace()
-    }
-
-    return Result.failure(
-      RequestException(
-        message = "Unable to fetch account",
-        code = HttpURLConnection.HTTP_INTERNAL_ERROR,
+      Result.failure(
+        RequestException(
+          message = th.message ?: "",
+          code = HttpURLConnection.HTTP_INTERNAL_ERROR,
+        )
       )
-    )
+    }
   }
 }
