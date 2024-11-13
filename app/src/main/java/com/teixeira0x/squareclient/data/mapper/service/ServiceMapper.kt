72ce10a04ce0ee322fbd908dec2c7f992f6596e1
Felipe Teixeira <secretedit123e@gmail.com>
2024-11-12 21:33:35 -0300

    fix: ServiceStatus model import

--- app/src/main/java/com/teixeira0x/squareclient/data/mapper/service/ServiceMapper.kt
+++ app/src/main/java/com/teixeira0x/squareclient/data/mapper/service/ServiceMapper.kt
@@ -1,7 +1,7 @@
 package com.teixeira0x.squareclient.data.mapper.service
 
 import com.teixeira0x.squareclient.data.model.response.service.ServiceStatusResponse
-import com.teixeira0x.squareclient.domain.model.ServiceStatus
+import com.teixeira0x.squareclient.domain.model.service.ServiceStatus
 
 /**
  * Class with useful functions for mapping response models to domain models.
